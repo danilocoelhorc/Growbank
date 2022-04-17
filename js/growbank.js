984b2b56e753fcde5f7880ada8437d304f745bfc
Danilo Coelho <contato.danilocoelho@gmail.com>
2022-04-17 15:13:34 -0300

    tela pós login + info atualizando em tela

--- js/growbank.js
+++ js/growbank.js
@@ -1,18 +1,21 @@
-const atualizarSaldo = document.getElementById('calcularSaldo');
-const atualizarSaque = document.getElementById('calcularSaque');
-const atualizarDeposito = document.getElementById('calcularDeposito');
-const atualizarTransferencia = document.getElementById('calcularTransferir');
+//const calculate1 = document.getElementById('calcularSaldo');
+//const calculate3 = document.getElementById('calcularDeposito');
+//const calculate4 = document.getElementById('calcularTransferir');
 const menu2 = 3, validSenha = 0
 let saldo = 100, chequeespecial = 500.00
 
-    let saldoHmtl = document.getElementById('saldoHmtl');
-    saldoHmtl.innerHTML = saldo;
+  	let saldoHmtl = document.getElementById('saldoHmtl');
+	saldoHmtl.innerHTML = saldo
+
+	let saqueHtml = document.getElementById ('saqueHtml');
+	let depositoHtml = document.getElementById ('depositoHtml');
+	let transferenciaHtml = document.getElementById ('transferenciaHtml');
 
 
-	atualizarSaldo.addEventListener ('click', saldoAtual);
-	atualizarSaque.addEventListener ('click', saqueAtual);
-	atualizarDeposito.addEventListener ('click', depositoAtual);
-	atualizarTransferencia.addEventListener ('click', transferenciaAtual);
+	//calculate1.addEventListener ('click', calcularSaldo);
+	calcularSaque.addEventListener ('click', saqueAtual);
+	calcularDeposito.addEventListener ('click', depositoAtual);
+	calcularTransferir.addEventListener ('click', transferenciaAtual);
 
 	function saldoAtual (){	
 		document.write ("Seu saldo atual é de: ", + saldo + " growcoins")
@@ -21,31 +24,35 @@ let saldo = 100, chequeespecial = 500.00
 	function depositoAtual () {
 		const deposito = parseFloat(document.getElementById('deposito').value);
 		saldo = saldo + deposito
-		document.write ("<br>Deposito realizada com sucesso!<br>Seu saldo atual é de: " + saldo + " growcoins")
-		document.write ("<br>Você também tem disponível: " + chequeespecial + " em cheque especial")	
+		saldoHmtl.innerHTML = saldo
+		depositoHtml.innerHTML = ("Você fez um depósito de:" + deposito) 
+		//document.write ("<br>Deposito realizada com sucesso!<br>Seu saldo atual é de: " + saldo + " growcoins")
+		//document.write ("<br>Você também tem disponível: " + chequeespecial + " em cheque especial")	
 	}
 
 	function saqueAtual () {
 		const saque = parseFloat(document.getElementById('saque').value);
 		let validaSaque, taxa = 2.50
-		validaSaque = saque + taxa
+		validaSaque = saque + taxa	
 
 		
 		if (validaSaque > saldo + chequeespecial) {
-			document.write ("Saldo indisponivel para saque, tente novamente!")	
+			alert ("Saldo indisponivel para saque, tente novamente!")	
 		}
 		else if (validaSaque <= saldo) {
 			saldo = saldo - validaSaque
-			document.write ("<br>Saque realizada com sucesso!<br>Seu saldo atual é de: " + saldo + " growcoins <br>")	
+			saldoHmtl.innerHTML = saldo
+			saqueHtml.innerHTML = ("Você fez um saque de:" + saque) 
+			//document.write ("<br>Saque realizada com sucesso!<br>Seu saldo atual é de: " + saldo + " growcoins <br>")	
 		}
 		else if (validaSaque > saldo) {
 			validaSaque = validaSaque - saldo
 			chequeespecial = chequeespecial - validaSaque
 			saldo = 0
-			document.write ("<br>Saque realizada com sucesso! <br> Seu saldo atual é de: " + saldo + " growcoins <br>")
-			document.write ("<br>Você ainda tem disponível: " + chequeespecial + " em cheque especial <br>")
+			saqueHtml.innerHTML = saldo
+			//document.write ("<br>Saque realizada com sucesso! <br> Seu saldo atual é de: " + saldo + " growcoins <br>")
+			//document.write ("<br>Você ainda tem disponível: " + chequeespecial + " em cheque especial <br>")
 		}
-		
 	}
 	function transferenciaAtual () {
 		const transferencia = parseFloat(document.getElementById('transferencia').value);
@@ -63,3 +70,6 @@ let saldo = 100, chequeespecial = 500.00
 		document.write ("<br>Você também tem disponível: " + chequeespecial + " em cheque especial <br>")
 		}
 	}
+
+
+	
